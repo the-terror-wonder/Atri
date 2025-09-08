12ec610de0d0a95e32394029a303957ae0d03de8
Amit Kumar Panchayan <akpanchayan@gmail.com>
2025-09-09 03:04:51 +0530

    refactor(server): implement custom error handling middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 import classroomRoutes from './routes/classroomRoutes.js';
 
@@ -11,7 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
@@ -21,5 +21,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/classrooms', classroomRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
